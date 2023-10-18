104010986cad1f09cc1f9ff0de176fc0da02172e
unknown <yogapermanabudi@gmail.com>
2023-10-18 07:47:39 +0700

    level 3 - add hash for password

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ import express from "express"
 import bodyParser from "body-parser"
 import ejs from "ejs"
 import mongoose from "mongoose"
-import encrypt from "mongoose-encryption"
 import 'dotenv/config'
+import md5 from "md5"
 
 const app = express()
 
@@ -27,7 +27,6 @@ const userSchema = new mongoose.Schema({
 
 
 
-userSchema.plugin(encrypt, {secret:process.env.SECRET, encryptedFields: ['password']})// to encrypt password only !
 
 
 const User = new mongoose.model("User", userSchema)
@@ -50,7 +49,7 @@ app.get("/register", function(req,res){
 app.post("/register", function(req, res){
     const newUser = new User({
         email: req.body.username,
-        password: req.body.password
+        password: md5(req.body.password)
     })
 
     newUser.save().then(function(){
@@ -64,7 +63,7 @@ app.post("/register", function(req, res){
 
 app.post("/login", function(req,res){
     const username = req.body.username
-    const password = req.body.password
+    const password = md5(req.body.password)
 
     User.findOne({email: username}).then(function(foundUser){
         if (foundUser){
